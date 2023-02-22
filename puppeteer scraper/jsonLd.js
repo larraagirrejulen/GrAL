7ef431b746bdcb58081aec83849e0da62703f11b
Julen <79035803+larraagirrejulen@users.noreply.github.com>
2023-02-22 09:06:51 +0100

    json-ld gauza gehiago

--- puppeteer scraper/jsonLd.js
+++ puppeteer scraper/jsonLd.js
@@ -10,11 +10,28 @@ export default class jsonLd{
     };
     #outcomes = {
         "FAIL": {"id": "earl:failed", "type": "earl:Fail"},
-        "PASS": {"id": "earl:inapplicable", "type": "earl:NotApplicable"},
         "CANNOTTELL": {"id": "earl:failed", "type": "earl:Fail"},
     };
+    #techniques = {
+        "ARIA6": {"url": "https://www.w3.org/WAI/WCAG21/Techniques/aria/ARIA6", "sc": "1.1.1", "description": "Using aria-label to provide labels for objects"},
+        "ARIA5": {"url": "https://www.w3.org/WAI/WCAG21/Techniques/aria/ARIA5", "sc": "4.1.2", "description": "Using the title attribute to identify form controls when the label element cannot be used"},
+        "ARIA11": {"url": "https://www.w3.org/WAI/WCAG21/Techniques/aria/ARIA11", "sc": ["1.3.1", "1.3.6", "2.4.1"], "description": "Using ARIA landmarks to identify regions of a page"},
+        "ARIA7": {"url": "https://www.w3.org/WAI/WCAG21/Techniques/aria/ARIA7", "sc": "2.4.4", "description": "Using aria-labelledby for link purpose"},
+        "ARIA16": {"url": "https://www.w3.org/WAI/WCAG21/Techniques/aria/ARIA16", "sc": "4.2.1", "description": "Using aria-labelledby to provide a name for user interface controls"},
+        "": {"url": "", "sc": "", "description": ""},
+        "": {"url": "", "sc": "", "description": ""},
+        "": {"url": "", "sc": "", "description": ""},
+        "": {"url": "", "sc": "", "description": ""},
+        "": {"url": "", "sc": "", "description": ""},
+        "": {"url": "", "sc": "", "description": ""},
+    };
 
-    constructor(validator, evaluatedPageUrl){
+    constructor({validator, evaluatedPageUrl} = null, fileName = null){
+        
+        if(fileName != null){
+            this.#json = require('./' + fileName);
+            return;
+        }
         
         this.#json = {
             "@contest": {
@@ -39,8 +56,8 @@ export default class jsonLd{
         this.#elementTemplate = {
 			"auditID" : "????",
 			"earl:assertedBy" : {
-				"doap:name" : this.#validators[validator]["name"],
-				"@id" : this.#validators[validator]["url"], 
+				"doap:name" : this.#validators[validator].name,
+				"@id" : this.#validators[validator].url, 
 				"@type" : "earl:Software"
 			},
 			"earl:mode" : {
@@ -69,19 +86,36 @@ export default class jsonLd{
         return this.#json;
     }
 
-    addNewElement(outcome, techniqueId){ // State: fail, cannotell, pass
+    addNewElement(outcome, pointer = null, techniqueId, htmlElementPath){ // State: fail, cannotell, pass
 
         var element = Object.assign({}, this.#elementTemplate);
 
+        var outcomeId;
+        var outcomeType;
+
+        if(outcome != "PASS"){
+            outcomeId = this.#outcomes[outcome].id
+            outcomeType = this.#outcomes[outcome].type
+        }else{
+            if(pointer != null){
+                outcomeId = "earl:passed"
+                outcomeType = "earl:Pass"
+            } else{
+                outcomeId = "earl:inapplicable"
+                outcomeType = "earl:NotApplicable"
+            }
+        }
+        
+
         element["earl:result"] = {
-            "dcterms:description" : "Using label elements to associate text labels with form controls.", 
+            "dcterms:description" : this.#techniques[techniqueId].description, 
             "earl:info" : "WCAG 2.1: technique " + techniqueId, 
             "earl:outcome" : {
-                "@id" : "earl:" + this.#outcomes[outcome].id, 
-                "@type" : "earl:" + this.#outcomes[outcome].type, 
+                "@id" : outcomeId, 
+                "@type" : outcomeType, 
             }, 
             "earl:pointer" : {
-                "ptr:expression" : "//html/body/div/header/div[4]/div/div/div[1]/div/div/section/div/div[2]/div/form/fieldset/div[2]/select", 
+                "ptr:expression" : htmlElementPath, 
                 "ptr:namespace" : { 
                     "@id" : "http://www.w3.org/1999/xhtml", 
                     "@type" : "ptr:NamespaceMapping", 
